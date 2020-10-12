b659ed269bce1dcc8dcea83c9a602c0b7fed0553
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-12 11:17:18 +0530

    Storing in Dictionary and Showing singlet percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -2,9 +2,29 @@
 
 echo "Welcome to the Flip Coin Combination"
 
-x=$(( RANDOM%2 ))
-if [ $x -eq 1 ]; then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A dict
+h=0
+t=0
+for((i=0;i<20;i++))
+do
+	x=$(( RANDOM%2 ))
+	if [ $x -eq 1 ]; then
+		echo "Heads"
+		dict[$i]="H"
+		((h++))
+	else
+		echo "Tails"
+		dict[$i]="T"
+		((t++))
+	fi
+done
+echo "${!dict[@]}"
+echo "${dict[@]}"
+
+echo $h
+echo $t
+perh=$( echo $h $t | awk "BEGIN {print $h/$t*100}" )
+pert=$( echo $h $t | awk "BEGIN {print $t/$h*100}" )
+
+echo $perh
+echo $pert
