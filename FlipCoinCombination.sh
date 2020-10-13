ec13bed384a6a4f1f58abe4d80fcf7e73c77d478
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-13 12:48:53 +0530

    Triplet added to the Dictionary and shows Triplet Percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -4,6 +4,8 @@ echo "Welcome to the Flip Coin Combination"
 
 declare -A dict
 declare -A dictD
+declare -A dictT
+
 h=0
 t=0
 
@@ -77,3 +79,85 @@ echo "Percentage of HH: " $perhh
 echo "Percentage of HT: " $perht
 echo "Percentage of TH: " $perth
 echo "Percentage of TT: " $pertt
+
+
+hhh=0
+htt=0
+hht=0
+hth=0
+
+ttt=0
+thh=0
+tht=0
+tth=0
+
+#dictT = dictionary with Triplets
+for((i=0;i<20;i++))
+do
+   y=$((1+RANDOM%8))
+   if [ $y -eq 1 ]; then
+#     echo "HHH"
+      dictT[$i]="HHH"
+      ((hhh++))
+   elif [ $y -eq 2 ]; then
+#     echo "HHT"
+      dictT[$i]="HHT"
+      ((hht++))
+   elif [ $y -eq 3 ]; then
+#     echo "HTH"
+      dictT[$i]="HTH"
+      ((hth++))
+   elif [ $y -eq 4 ]; then
+#     echo "HTT"
+      dictT[$i]="HTT"
+      ((htt++))
+   elif [ $y -eq 5 ]; then
+#     echo "THH"
+      dictT[$i]="THH"
+      ((thh++))
+   elif [ $y -eq 6 ]; then
+#     echo "THT"
+      dictT[$i]="THT"
+      ((tht++))
+   elif [ $y -eq 7 ]; then
+#     echo "TTH"
+      dictT[$i]="TTH"
+      ((tth++))
+   elif [ $y -eq 8 ]; then
+#     echo "TTT"
+      dictT[$i]="TTT"
+      ((ttt++))
+   fi
+
+done
+echo "Triplet Dictionary:"
+echo "${dictT[@]}"
+echo "The value: "
+echo "HHH: " $hhh
+echo "HHT: " $hht
+echo "HTH: " $hth
+echo "HTT: " $htt
+echo "THH: " $thh
+echo "THT: " $tht
+echo "TTH: " $tth
+echo "TTT: " $ttt
+
+
+perhhh=$( echo $hhh  | awk "BEGIN {print $hhh/20*100}" )
+perhht=$( echo $hht  | awk "BEGIN {print $hht/20*100}" )
+perhth=$( echo $hth  | awk "BEGIN {print $hth/20*100}" )
+perhtt=$( echo $htt  | awk "BEGIN {print $htt/20*100}" )
+perthh=$( echo $thh  | awk "BEGIN {print $thh/20*100}" )
+pertht=$( echo $tht  | awk "BEGIN {print $tht/20*100}" )
+pertth=$( echo $tth  | awk "BEGIN {print $tth/20*100}" )
+perttt=$( echo $ttt  | awk "BEGIN {print $ttt/20*100}" )
+
+
+echo "Percentage of HHH: " $perhhh
+echo "Percentage of HHT: " $perhht
+echo "Percentage of HTH: " $perhth
+echo "Percentage of HTT: " $perhtt
+echo "Percentage of THH: " $perthh
+echo "Percentage of THT: " $pertht
+echo "Percentage of TTH: " $pertth
+echo "Percentage of TTT: " $perttt
