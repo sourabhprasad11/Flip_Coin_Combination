3f381700e2ecbcf781cfaafb6742baf4ae3c4e04
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-16 13:20:38 +0530

    Altered- Singlet storage in Dictionary

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -3,28 +3,34 @@
 echo "Welcome to the Flip Coin Combination"
 
 declare -A dict
+
+function singlet(){
 h=0
 t=0
 for((i=0;i<20;i++))
 do
 	x=$(( RANDOM%2 ))
 	if [ $x -eq 1 ]; then
-		echo "Heads"
 		dict[$i]="H"
 		((h++))
 	else
-		echo "Tails"
 		dict[$i]="T"
 		((t++))
 	fi
 done
-echo "${!dict[@]}"
+
 echo "${dict[@]}"
 
-echo $h
-echo $t
 perh=$( echo $h $t | awk "BEGIN {print $h/$t*100}" )
 pert=$( echo $h $t | awk "BEGIN {print $t/$h*100}" )
 
-echo $perh
-echo $pert
+echo "H%: " $perh
+echo "T%: " $pert
+}
+
+function main(){
+
+	singlet
+
+}
+main
